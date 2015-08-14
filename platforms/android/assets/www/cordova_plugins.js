84fcfbfe054c2f6d2775667bae5993ddc64bb702
GathoMauricio <mauricio2769@gmail.com>
2015-08-14 07:35:48 -0500

    Ok

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,29 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.printer/www/printer.js",
+        "id": "de.appplant.cordova.plugin.printer.Printer",
+        "clobbers": [
+            "plugin.printer",
+            "cordova.plugins.printer"
+        ]
+    },
+    {
+        "file": "plugins/com.darktalker.cordova.screenshot/www/Screenshot.js",
+        "id": "com.darktalker.cordova.screenshot.screenshot",
+        "merges": [
+            "navigator.screenshot"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "de.appplant.cordova.plugin.printer": "0.7.1",
+    "com.darktalker.cordova.screenshot": "0.1.1"
 }
 // BOTTOM OF METADATA
 });
--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,3 +1,4 @@
+
 var app = {
     initialize: function() {
         this.bindEvents();
@@ -10,10 +11,31 @@ var app = {
         $.post("http://sacygrestaurantes.com/pop/index.php",{},function(data){
                $("#principal").html(data);
         });
+        cordova.plugins.printer.isAvailable(
+            function (isAvailable) {
+                //alert(isAvailable ? 'Service is available' : 'Service NOT available');
+
+            }
+        );
     },
     receivedEvent: function(id) {
         console.log('Received Event: ' + id);
     }
 };
+function imprimir()
+{
+    //Tomando captura
+    navigator.screenshot.URI(function(error,res){
+      if(error){
+        console.error(error);
+      }else{
+      //obteniendo url temporal
+        html = '<img src="'+res.URI+'">';
+        //enviando al servicio de impresión
+        cordova.plugins.printer.print(html, 'Document.html', function () {
+         });
+      }
+    },50);
 
+}
 app.initialize();
